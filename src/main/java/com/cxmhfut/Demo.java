17eb7dfc245e43e0ce4b00bed284ac8c7c4c22c8
cxmhfut <912750125@qq.com>
2018-11-21 20:56:26 +0800

    文本分类

--- src/main/java/com/cxmhfut/Demo.java
+++
@@ -1,9 +0,0 @@
-package com.cxmhfut;
-
-import com.hankcs.hanlp.HanLP;
-
-public class Demo {
-    public static void main(String[] args) {
-        System.out.println(HanLP.segment("你好，欢迎使用HanLP汉语处理包！"));
-    }
-}
